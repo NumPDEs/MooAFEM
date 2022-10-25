d76b0e9752e43dd946066024605d84237ba6cb8f
Julian Streitberger <julian.streitberger@tuwien.ac.at>
2022-10-25 16:24:32 +0200

    Bring PCG to multigrid solver format

--- lib/solvers/AdditiveSchwartzPcg.m
+++ lib/solvers/AdditiveSchwartzPcg.m
@@ -9,57 +9,63 @@ classdef AdditiveSchwartzPcg < PcgSolver
     end
     
     properties (Access=protected)
-        Acoarse
-        fine2coarse
-        intergridMatrix
-        oldFreeDofs
-        oldPatchAreas
-        fes
-        D
+        blf
+        hoFes
+        loFes
+        p1Matrix
+        p1Smoother
         P
+        intergridMatrix
+        freeVertices
+        freeVerticesOld
+        changedPatches
+        highestOrderIsOne
+        patchwiseA
     end
     
     %% methods
     methods (Access=public)
-        function obj = AdditiveSchwartzPcg(P)
-            assert(isa(P, 'Prolongation'), 'P must be an instance of Prolongation')
-            assert(isa(P.fes.finiteElement, 'LowestOrderH1Fe'), 'Only lowest-order H1 finite elements are supported')
-            
+        function obj = AdditiveSchwartzPcg(fes, blf)
+            arguments
+                fes FeSpace
+                blf BilinearForm
+            end
             obj = obj@PcgSolver();
-            obj.fes = P.fes;
-            obj.P = P;
+            
+            obj.highestOrderIsOne = (fes.finiteElement.order == 1);
             obj.nLevels = 0;
-            obj.fine2coarse = [];
-            obj.D = {};
-            obj.Acoarse = [];
+            
+            mesh = fes.mesh;
+            obj.loFes = FeSpace(mesh, LowestOrderH1Fe(), 'dirichlet', ':');
+            obj.hoFes = fes;
+            obj.P = LoFeProlongation(obj.loFes);
+            obj.blf = blf; % TODO: check coefficients of blf for compatibility with theoretical results!
+            
+            obj.listenerHandle = mesh.listener('IsAboutToRefine', @obj.getChangedPatches);
             obj.intergridMatrix = [];
         end
         
         function setupLinearSystem(obj, A, b, x0)
-            mesh = obj.fes.mesh;
-            freeDofs = getFreeDofs(obj.fes);
-            patchAreas = computePatchAreas(mesh);
-            
-            if obj.nLevels == 0
-                % on coarsest level store whole matrix
-                obj.Acoarse = A;
-            else    
-                % find nodes where patch has changed to ensure that no node gets
-                % considered twice (new nodes are just appended)
-                obj.oldPatchAreas(mesh.nCoordinates) = 0;
-                stableNodes = abs(obj.oldPatchAreas - patchAreas) < 2*eps;
-                obj.intergridMatrix{obj.nLevels} = obj.P.matrix(freeDofs, obj.oldFreeDofs);
-                
-                % on every other level only store inverse of diagonal
-                % (only on free dofs and dofs where refinement happened;
-                % we heavily use that dofs and nodes coincide, here)
-                obj.D{obj.nLevels} = 1./full(diag(A));
-                obj.D{obj.nLevels}(stableNodes(freeDofs)) = 0;
-            end
-            
-            obj.oldFreeDofs = freeDofs;
-            obj.oldPatchAreas = patchAreas;
             obj.nLevels = obj.nLevels + 1;
+
+            L = obj.nLevels;
+            obj.freeVerticesOld = obj.freeVertices;
+            obj.freeVertices = getFreeDofs(obj.loFes);
+            
+            if obj.highestOrderIsOne
+                obj.p1Matrix{L} = A;
+            else
+                obj.p1Matrix{L} = assemble(obj.blf, obj.loFes);
+                obj.p1Matrix{L} = obj.p1Matrix{L}(obj.freeVertices, obj.freeVertices);
+            end
+
+            if L >= 2
+                obj.intergridMatrix{L} = obj.P.matrix(obj.freeVertices, obj.freeVerticesOld);
+                obj.changedPatches{L} = find(obj.changedPatches{L}(obj.freeVertices));
+                if ~obj.highestOrderIsOne
+                    obj.patchwiseA = assemblePatchwise(obj.blf, obj.hoFes);
+                end
+            end
             
             setupLinearSystem@PcgSolver(obj, A, b, x0);
         end
@@ -84,6 +90,47 @@ classdef AdditiveSchwartzPcg < PcgSolver
             end
             
             Cx = y{end};
+        end
+    end
+
+    methods (Access=protected)
+        % callback function to be executed before mesh refinement:
+        % -> patches change for all vertices adjacent to refined edges and
+        %    all new vertices
+        function getChangedPatches(obj, mesh, bisecData)
+            nCOld = mesh.nCoordinates;
+            nCNew = mesh.nCoordinates + nnz(bisecData.bisectedEdges) + ...
+                bisecData.nRefinedElements'*bisecData.nInnerNodes;
+            bisectedEdgeNodes = unique(mesh.edges(:,bisecData.bisectedEdges));
+            obj.changedPatches{obj.nLevels+1} = false(nCNew, 1);
+            idx = [unique(bisectedEdgeNodes); ((nCOld+1):nCNew)'];
+            obj.changedPatches{obj.nLevels+1}(idx) = true;
+        end
+        
+        function rho = p1LocalSmoothing(obj, k, res)
+            idx = obj.changedPatches{k};
+            rho = zeros(size(res));
+            rho(idx,:) = obj.p1Smoother{k}(idx).*res(idx,:);
+        end
+        
+        function rho = hoGlobalSmoothing(obj, res)
+            rho = obj.patchwiseA \ res;
+        end
+        
+        function y = projectFrom1toP(obj, x)
+            if obj.highestOrderIsOne
+                y = x;
+            else
+                y = interpolateFreeData(x, obj.loFes, obj.hoFes);
+            end
+        end
+        
+        function y = projectFromPto1(obj, x)
+            if obj.highestOrderIsOne
+                y = x;
+            else
+                y = interpolateFreeData(x, obj.hoFes, obj.loFes);
+            end
         end
     end
 end
