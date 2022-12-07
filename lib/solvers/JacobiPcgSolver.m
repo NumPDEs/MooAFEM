2a6f1cb4bbcf4d71250dad7c7330031931378a44
Julian Streitberger <julian.streitberger@tuwien.ac.at>
2022-12-07 13:33:37 +0100

    Add the Jacobi preconditioner for higher p (p1 not working)

--- lib/solvers/JacobiPcgSolver.m
+++ lib/solvers/JacobiPcgSolver.m
@@ -1,22 +1,50 @@
-% JacobiPcgSolver (subclass of PcgSolver) inear equations iteratively using
-%   the CG method with diagonal preconditioner.
+% JacobiPcgSolver (subclass of PcgSolver) linear equations iteratively using
+% the CG method with diagonal preconditioner.
 
 classdef JacobiPcgSolver < PcgSolver
     %% properties
     properties (GetAccess=public,SetAccess=protected)
-        C (:,1) double
+        C
+        hoFes
+        blf
+
+        highestOrderIsOne
     end
     
     %% methods
     methods (Access=public)
         % preconditioner action: inverse of diagonal
-        function setupLinearSystem(obj, A, b, x0)
-            obj.C = 1./full(diag(A));
-            setupLinearSystem@PcgSolver(obj, A, b, x0)
+        function obj = JacobiPcgSolver(fes, blf)
+            arguments
+                fes FeSpace
+                blf BilinearForm
+            end
+            obj = obj@PcgSolver();
+
+            obj.highestOrderIsOne = (fes.finiteElement.order == 1);
+
+            obj.hoFes = fes;
+            obj.blf = blf; % TODO: check coefficients of blf for compatibility with theoretical results!
+            
+            %obj.listenerHandle = mesh.listener('IsAboutToRefine', @obj.getChangedPatches);
+        end
+        function setupSystemMatrix(obj, A)
+            
+            if obj.highestOrderIsOne
+                obj.C = full(diag(A)).^(-1);
+            else
+                obj.C = assemblePatchwise(obj.blf, obj.hoFes, ':');
+            end
+            
+            setupSystemMatrix@PcgSolver(obj, A);
+        end
+           
+        function setupRhs(obj, b, x0)
+            setupRhs@PcgSolver(obj, b, x0);
         end
         
         function Cx = preconditionAction(obj, x)
-            Cx = obj.C .* x;
+            Cx = obj.C \ x;
         end
     end
 end
