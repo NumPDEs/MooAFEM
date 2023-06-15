aa7920e90463037e27bb064ea51b43f04ca2fbaa
Julian Streitberger <julian.streitberger@tuwien.ac.at>
2023-06-15 16:38:52 +0200

    Update inclusionMatrix (working but error)

--- test.m
+++ test.m
@@ -2,31 +2,45 @@ mesh = Mesh.loadFromGeometry('unitsquare');
 mesh.refineUniform();
 trafo = getAffineTransformation(mesh);
 
-fes1 = FeSpace(mesh, HigherOrderH1Fe(2), 'dirichlet', ':');
-fes2 = FeSpace(mesh, HigherOrderH1Fe(4), 'dirichlet', ':');
+fes1 = FeSpace(mesh, HigherOrderH1Fe(4), 'dirichlet', ':');
+fes2 = FeSpace(mesh, HigherOrderH1Fe(2), 'dirichlet', ':');
 
-testMatrix = ones(getDofs(fes1).nDofs, 1);
+testMatrix = ones(getDofs(fes2).nDofs, 1);
 inclusionMatrix = FeProlongation(fes1, fes2);
+inclusionMatrix = full(inclusionMatrix);
+Vertices = 1:getDofs(fes1).nDofs;
+inclusionMatrix2 = interpolateData(eye(length(Vertices)), fes1, fes2)';
+
 
 function inclusionMatrix = FeProlongation(fromFes, toFes)
+    %Create the prolongation matrix on the unit triangle
     unittriangle = Mesh.loadFromGeometry('unittriangle');
-    fromFesUnitTriangle = FeSpace(unittriangle, HigherOrderH1Fe(fromFes.finiteElement.order));
-    toFesUnitTriangle = FeSpace(unittriangle, HigherOrderH1Fe(toFes.finiteElement.order));
+    fromFesUnitTriangle = FeSpace(unittriangle, ...
+                             HigherOrderH1Fe(fromFes.finiteElement.order));
+    toFesUnitTriangle = FeSpace(unittriangle, ...
+                               HigherOrderH1Fe(toFes.finiteElement.order));
     Vertices = 1:getDofs(fromFesUnitTriangle).nDofs;
-    unitTriangleInclusionMatrix = interpolateData(eye(length(Vertices)), ...
-                                   fromFesUnitTriangle, toFesUnitTriangle);
+    unitTriangleInclusionMatrix = ...
+        permute(interpolateData(eye(length(Vertices)),...
+                           fromFesUnitTriangle, toFesUnitTriangle), [2 1]);
 
+    % Get dofs of the finite element spaces on the meshes
     fromFesDofs = getDofs(fromFes);
     toFesDofs = getDofs(toFes);
 
-    [row, ~] = getLocalDofs(size(fromFesDofs.element2Dofs, Dim.Vector));
-    [~, column] = getLocalDofs(size(toFesDofs.element2Dofs, Dim.Vector));
+    % Copy the matrix from the unit triangle for each element
+    mat = repmat(unitTriangleInclusionMatrix, ...
+                 [1, 1, fromFes.mesh.nElements]);
 
-    I = fromFesDofs.element2Dofs(row, :);
-    J = toFesDofs.element2Dofs(:, column);
-    sub2ind(size(I), I(:, :));
+    % Create index sets for the accumarray
+    temp = fromFesDofs.element2Dofs(:, :);
+    temp2 = toFesDofs.element2Dofs(:, :);
+    I = repmat(permute(temp, [1 3 2]), [1, size(temp2, 1), 1]);
+    J = repmat(permute(temp2, [3 1 2]), [size(temp, 1), 1, 1]);
+    ind = [I(:), J(:)];
     
-    inclusion = accumarray([I, J], unitTriangleInclusionMatrix, [], @mean, [], true);
+    inclusionMatrix = ...
+      accumarray(ind, mat(:), [], @mean, [], true);
 end
 
 function [localI, localJ] = getLocalDofs(n)
@@ -45,3 +59,5 @@ function interpolatedData = interpolateData(data, fromFes, toFes)
         interpolatedData(:,k) = wholeData(Dofs);
     end
 end
+
+
