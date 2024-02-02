e92d3e4a58f09d07f2c31958dbd98f2e879f3aba
Philipp Bringmann <philipp.bringmann@tuwien.ac.at>
2024-02-02 19:57:50 +0100

    Script for timing quickMark

---
+++ tests/TimeQuickMark.m
@@ -0,0 +1,24 @@
+function leveldata = TimeQuickMark(nLevel)
+
+    N = cumprod(repmat(4, nLevel, 1));
+
+    eta = rand(N(end), 1);
+    theta = 0.5;
+
+    leveldata = LevelData();
+
+    for j = 1:nLevel
+        tic;
+        quickMark(eta(1:N(j)), theta);
+        runtimeQuick = toc;
+        tic;
+        markDoerflerSorting(eta(1:N(j)), theta);
+        runtimeSort = toc;
+        leveldata.append("jLevel", j, "N", N(j));
+        leveldata.setTime(leveldata.nLevel, "runtimeQuick", runtimeQuick, ...
+                          "runtimeSort", runtimeSort);
+        leveldata.printLevel();
+    end
+
+
+end
