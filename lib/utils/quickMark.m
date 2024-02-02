3cb2873136654fab38a7f8d2c2f1cfe564c9f2ec
Philipp Bringmann <philipp.bringmann@tuwien.ac.at>
2024-02-02 20:34:22 +0100

    Remove local function in quickMark

--- lib/utils/quickMark.m
+++ lib/utils/quickMark.m
@@ -59,18 +59,3 @@ function marked = quickMark(eta, theta)
     marked = reshape(permutation(1:nSelected), [], 1);
 end
 
-
-function [m, position] = quickMedian(x, varargin)
-    if nargin < 2
-        varargin = {};
-    end
-    % Compute median (MATLAB built-in with linear complexity)
-    % m = median(x, varargin{:});
-    m = matlab.internal.math.columnmedian(x, false);
-    % Choose larger one of two values closest to the median
-    position = find(abs(abs(x - m) - min(abs(x - m))) < 1e-12);
-    [~, idx] = min(x(position));
-    position = position(idx);
-
-end
-
